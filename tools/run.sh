95dc16319b22f70b816af315708c93afb1317b4a
chenshi3 <sczcl10@mail.ustc.edu.cn>
2023-03-31 21:19:52 +0800

    Update shell files

--- tools/run.sh
+++
@@ -1,2 +0,0 @@
-bash scripts/dist_train.sh 8 --cfg_file ./cfgs/dsvt_models/dsvt_plain_12eD5.yaml --ckpt_save_interval 12 --workers 1 \
---sync_bn --batch_size 8 --epochs 12 --logger_iter_interval 1000
--- tools/run2.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -gt 0 ]; then
-  echo "Wating for process $1 finish ..."
-  tail --pid=$1 -f /dev/null
-  sleep 60s
-fi
-
-bash scripts/dist_train.sh 8 --cfg_file ./cfgs/dsvt_models/dsvt_3D_D512e.yaml --ckpt_save_interval 12 --workers 1  \
---sync_bn --batch_size 8 --epochs 12 --logger_iter_interval 1000 --extra_tag D512eb8
