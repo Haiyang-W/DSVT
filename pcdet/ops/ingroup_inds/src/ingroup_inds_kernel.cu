99fac452bbe5600f8c19db25b51d98d9b74d259f
Haiyang-W <wanghaiyang672@gmail.com>
2023-06-29 00:14:22 +0800

    fix bug of compiling ingroup_inds op

--- pcdet/ops/ingroup_inds/src/ingroup_inds_kernel.cu
+++ pcdet/ops/ingroup_inds/src/ingroup_inds_kernel.cu
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <torch/serialize/tensor.h>
-#include <torch/extension.h>
+// #include <torch/extension.h>
 #include <torch/types.h>
 #include "cuda_fp16.h"
 // #include "error.cuh"
